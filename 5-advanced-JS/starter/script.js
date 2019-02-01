190dbc9f48f77a409a7dfcf4ddc3af68ff376b06
jstricklin89 <jstricklin89@gmail.com>
2019-02-01 10:01:41 -0600

    added prototype to class

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -12,5 +12,16 @@ var Person = function(name, yearOfBirth, job) {
   this.job = job;
 };
 
+Person.prototype.calculateAge = function() {
+  console.log(2016 - this.yearOfBirth);
+};
+
 //instantiation
+
 var john = new Person("John", 1990, "teacher");
+var jane = new Person("Jane", 1969, "designer");
+var mark = new Person("Mark", 1948, "retired");
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
