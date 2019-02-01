6e2e44d8cd19d76cbfbb8a64923d44c1a464466e
jstricklin89 <jstricklin89@gmail.com>
2019-02-01 09:55:15 -0600

    first constructor lesson

---
+++ 5-advanced-JS/starter/script.js
@@ -0,0 +1,16 @@
+// Function constructor
+
+var john = {
+  name: "John",
+  yearOfBirth: 1990,
+  job: "teacher"
+};
+
+var Person = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+//instantiation
+var john = new Person("John", 1990, "teacher");
