8eb8b7998ab8fb559b95c3923db8086a4067a4bf
jstricklin89 <jstricklin89@gmail.com>
2019-02-01 10:37:58 -0600

    created my own class/prototype to test

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,27 +1,35 @@
 // Function constructor
-
-var john = {
-  name: "John",
-  yearOfBirth: 1990,
-  job: "teacher"
-};
-
 var Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
 };
-
+//prototype added to constructor
 Person.prototype.calculateAge = function() {
   console.log(2016 - this.yearOfBirth);
 };
-
-//instantiation
-
+//instantiation, creating instances
 var john = new Person("John", 1990, "teacher");
 var jane = new Person("Jane", 1969, "designer");
 var mark = new Person("Mark", 1948, "retired");
+//executing code
+// john.calculateAge();
+// jane.calculateAge();
+// mark.calculateAge();
+//-----------------try on own now
+var Car = function(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+};
 
-john.calculateAge();
-jane.calculateAge();
-mark.calculateAge();
+Car.prototype.age = function() {
+  console.log(Car.prototype);
+  console.log(2019 - this.year);
+};
+
+var corvette = new Car("Chevrolet", "Corvette", 2010);
+var mustang = new Car("Ford", "Mustang", 1969);
+
+corvette.age();
+mustang.age();
