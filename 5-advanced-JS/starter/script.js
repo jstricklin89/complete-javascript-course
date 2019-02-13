32750459ece5c4763d874b2ba48ce97214be70dd
jstricklin89 <jstricklin89@gmail.com>
2019-02-13 13:30:48 -0600

    added bind, call, apply

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -163,12 +163,58 @@ var jane = Object.create(personProto, {
 //   console.log(score >= 5);
 // }
 
-(function() {
-  var score = Math.random() * 10;
-  console.log(score >= 5);
-})();
+// (function() {
+//   var score = Math.random() * 10;
+//   console.log(score >= 5);
+// })();
 
-(function(goodLuck) {
-  var score = Math.random() * 10;
-  console.log(score >= 5 - goodLuck);
-})(5);
+// (function(goodLuck) {
+//   var score = Math.random() * 10;
+//   console.log(score >= 5 - goodLuck);
+// })(5);
+
+////////////////////////////////////////
+//Bind, call, apply
+
+var john = {
+  name: "John",
+  age: 26,
+  job: "teacher",
+  presentation: function(style, timeOfDay) {
+    if (style === "formal") {
+      console.log(
+        "Good " +
+          timeOfDay +
+          ", Ladies and gentlemen! I'm " +
+          this.name +
+          ", I'm a " +
+          this.job +
+          " and I'm " +
+          this.age +
+          " years old."
+      );
+    } else if (style === "friendly") {
+      console.log(
+        "Hey! What's up? I'm " +
+          this.name +
+          ", I'm a " +
+          this.job +
+          " and I'm " +
+          this.age +
+          " years old. Have a nice " +
+          timeOfDay +
+          "."
+      );
+    }
+  }
+};
+
+var emily = {
+  name: "Emily",
+  age: 35,
+  job: "designer"
+};
+
+john.presentation("formal", "morning");
+
+john.presentation.call(emily, "friendly", "afternoon");
